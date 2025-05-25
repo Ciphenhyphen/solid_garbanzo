ea14ad9da2d5470f688ffd7cc04b3a939b5a04cb
Ciphenhyphen <leonardopena004@gmail.com>
2025-05-24 20:27:59 -0500

    Add files via upload

---
+++ pypractice_1.py
@@ -0,0 +1,6 @@
+user_name = input("Before I begin, what's your name?\n")
+greet_1 = f"Hello {user_name}, welcome to Python"
+print(greet_1)
+
+
+message = 'This is a string, but do not use more than the two quotations you have or this will cause an error    '
